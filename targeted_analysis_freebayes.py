e8a4413962c6f503922ce8ecb68746141eceb67a
EC2 Default User <ec2-user@ip-172-31-23-51.ap-southeast-1.compute.internal>
2016-04-11 18:52:51 +0000

    Changes

--- targeted_analysis_freebayes.py
+++ targeted_analysis_freebayes.py
@@ -17,10 +17,13 @@ current_instance_id = call("wget -q -O - http://instance-data/latest/meta-data/i
 
 min_depth = 100
 min_qual  = 30
+num_cores = 16
+sambamba_mem = 10
+
 opt_list = sys.argv[1:]
 #If no sysarg apart from filename, will fail
 try:
-    opts, args = getopt.getopt(opt_list,"sqd",["shutdown", "min_qual", "min_depth"])
+    opts, args = getopt.getopt(opt_list,"sqdtm",["sambamba_mem", "num_cores", "shutdown", "min_qual", "min_depth"])
 except getopt.GetoptError:
     print 'Bad inputs'
     quit()    
@@ -33,6 +36,10 @@ for opt, arg in opts:
         min_qual = arg
     elif opt in ("-d", "--min_depth"):
         min_depth = arg
+    elif opt in ("-t", "--num_cores"):
+        num_cores = arg
+    elif opt in ("-m", "--sambamba_mem"):
+        sambamba_mem = arg
 
 #Get reference sequences
 if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa.gz"):
@@ -94,7 +101,7 @@ for line in sample_list:
         call(command, shell = True)
 	#Align and call variants
         print "Aligning for " + sample_name
-        command = "docker run --rm=true -v /home/ec2-user:/home wengkhong/speedseq code/speedseq/bin/speedseq align -o /home/" + sample_name + "/" + sample_name + " -R \"@RG\\tID:" + sample_name + "\\tSM:" + sample_name + "\\tLB:lib1\" -t16 -T /home/" + sample_name + "/" + sample_name + "_temp -M 10 /home/ref/hs37d5.fa /home/" + sample_name + "/" + os.path.basename(file1) + " /home/" + sample_name + "/" + os.path.basename(file2)
+        command = "docker run --rm=true -v /home/ec2-user:/home wengkhong/speedseq code/speedseq/bin/speedseq align -o /home/" + sample_name + "/" + sample_name + " -R \"@RG\\tID:" + sample_name + "\\tSM:" + sample_name + "\\tLB:lib1\" -t" + str(num_cores) + " -T /home/" + sample_name + "/" + sample_name + "_temp -M " + str(sambamba_mem) + " /home/ref/hs37d5.fa /home/" + sample_name + "/" + os.path.basename(file1) + " /home/" + sample_name + "/" + os.path.basename(file2)
         print command
         call(command, shell = True)
         #exit()
