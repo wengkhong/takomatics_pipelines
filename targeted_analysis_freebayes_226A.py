264e41a8457c6b9b72c57ad83b7e87afb065535f
EC2 Default User <ec2-user@ip-172-31-23-51.ap-southeast-1.compute.internal>
2016-04-12 17:26:52 +0000

    new file

---
+++ targeted_analysis_freebayes_226A.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+import os.path
+from subprocess import call
+import subprocess
+import csv
+import shutil
+import sys, getopt
+import getopt
+import sys
+
+sample_list_path = 's3://takomaticsdata/226A_Pipeline_Test/N226A_SampleSheet.csv'
+target_region_path = 's3://takomaticsdata/SureSelect_V5_plusUTRs_hs37d5.bed'
+parent_path = os.path.dirname(sample_list_path)
+
+#Get instance id
+current_instance_id = call("wget -q -O - http://instance-data/latest/meta-data/instance-id", shell = True)
+
+min_depth = 100
+min_qual  = 30
+num_cores = 16
+sambamba_mem = 10
+
+opt_list = sys.argv[1:]
+#If no sysarg apart from filename, will fail
+try:
+    opts, args = getopt.getopt(opt_list,"sqdtm",["sambamba_mem", "num_cores", "shutdown", "min_qual", "min_depth"])
+except getopt.GetoptError:
+    print 'Bad inputs'
+    quit()    
+
+shutdown_flag = False
+for opt, arg in opts:
+    if opt in ("-s", "--shutdown"):
+        shutdown_flag = True
+    elif opt in ("-q", "--min_qual"):
+        min_qual = arg
+    elif opt in ("-d", "--min_depth"):
+        min_depth = arg
+    elif opt in ("-t", "--num_cores"):
+        num_cores = arg
+    elif opt in ("-m", "--sambamba_mem"):
+        sambamba_mem = arg
+
+#Get reference sequences
+if not os.path.isfile("/home/ec2-user/ref/hs37d5.fa.gz"):
+	command = "aws s3 cp s3://takomaticsdata/reference_genomes/hg19/ /home/ec2-user/ref --recursive --exclude \"*\" --include \"hs37d5*\""
+	print(command)
+	call(command, shell = True)
+
+command = "docker pull wengkhong/speedseq"
+call(command, shell = True)
+command = "docker pull wengkhong/vcflib"
+call(command, shell = True)
+
+#Get Boto3. This is not currently in use but may be in the future
+call("sudo pip install boto3", shell = True)
+import boto3
+#Get sample list and target region
+call("aws s3 cp " + sample_list_path + " .", shell = True)
+call("aws s3 cp " + target_region_path + " .", shell = True)
+with open(os.path.basename(sample_list_path), 'r') as csv:
+	sample_list = [line.strip().split(',') for line in csv]
+
+print("Length of sample list: " + str(len(sample_list)-1))
+
+#This is a helper function to check if a file already exists in S3 or not. Primarily used to avoid reprocessing when resuming from incomplete runs
+def checkFileInS3(command):
+	proc = subprocess.Popen(command, stdout = subprocess.PIPE, shell = True)
+        (out,err) = proc.communicate()
+        #print out
+        if out:
+		return True;
+        if not out:
+		return False;
+
+
+for line in sample_list:
+	file1 = str(line[0])
+	file2 = str(line[1])
+	sample_name = str(line[2])
+	if file1 == "File1":
+		continue
+	#Check if output for this sample already exists. Skip if so
+        command = "aws s3 ls " + parent_path + "/" + sample_name + "/" + sample_name + ".vcf"
+	if(checkFileInS3(command)):
+		print "Sample " + sample_name + " already done. Skipping"
+		continue
+	############################################################
+	print "Currently processing " + sample_name
+	#Create folder
+        if not os.path.isdir(sample_name):
+            os.mkdir(sample_name)
+            
+	#Goto folder
+        os.chdir(sample_name)
+        #Get fastq files
+        print "Getting fastq files"
+        command = "aws s3 cp " + file1 + " ."
+        call(command, shell = True)
+        command = "aws s3 cp " + file2 + " ."
+        call(command, shell = True)
+	#Align and call variants
+        print "Aligning for " + sample_name
+        command = "docker run --rm=true -v /home/ec2-user:/home wengkhong/speedseq code/speedseq/bin/speedseq align -o /home/" + sample_name + "/" + sample_name + " -R \"@RG\\tID:" + sample_name + "\\tSM:" + sample_name + "\\tLB:lib1\" -t" + str(num_cores) + " -T /home/" + sample_name + "/" + sample_name + "_temp -M " + str(sambamba_mem) + " /home/ref/hs37d5.fa /home/" + sample_name + "/" + os.path.basename(file1) + " /home/" + sample_name + "/" + os.path.basename(file2)
+        print command
+        call(command, shell = True)
+        #exit()
+        print "Calling variants for " + sample_name
+        command = "docker run --rm=true -v /home/ec2-user:/home wengkhong/freebayes freebayes -f /home/ref/hs37d5.fa -b /home/" + sample_name + "/" + sample_name + ".bam -v /home/" + sample_name + "/" + sample_name + ".vcf --target /home/" + os.path.basename(target_region_path)
+        print command
+        call(command, shell = True)
+        #exit()
+        print "Filtering variants for " + sample_name
+        command = "docker run -it --rm=true -v /home/ec2-user/:/home wengkhong/vcflib vcflib/bin/vcffilter -f 'DP >" + str(min_depth - 1) + " & QUAL > " + str(min_qual - 1) + "' /home/" + sample_name + "/" + sample_name + ".vcf > " + sample_name + ".filtered.vcf"
+        print command
+        call(command, shell = True)
+        print "Getting variants in target region"
+        command = "docker run --rm=true -v /home/ec2-user:/home wengkhong/vcflib bedtools intersect -a /home/" + sample_name + "/" + sample_name + ".filtered.vcf -b /home/" + os.path.basename(target_region_path) + " > " + sample_name + ".filtered.target.vcf"
+        print command
+        call(command, shell = True)
+	#Upload results
+        print "Uploading results for " + sample_name
+        command = " aws s3 cp . " + parent_path + "/" + sample_name + " --recursive --exclude \"*discordants*\" --exclude \"*splitters*\" --exclude \"*fq.gz\" --exclude \"*fastq*\""
+        call(command, shell = True)
+	#Delete folder
+        os.chdir('..')
+        print "Cleaning up folder for " + sample_name
+        shutil.rmtree(sample_name)
+        #break
+        quit()
