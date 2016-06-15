8a5abe5fe22542acd32b10f2217d0b21baf3eae9
Lim Weng Khong <NUSSTF\gmslwkg@rozencompute.stf.nus.edu.sg>
2016-06-15 17:47:31 +0800

    Test

--- qdnaseq_workflow.py
+++ qdnaseq_workflow.py
@@ -8,6 +8,7 @@ import sys, getopt
 import getopt
 import sys
 import argparse
+import glob
 
 input_bamfile = '0315-0051-NHC-SNJ.bam'
 sambamba_threads = 10
@@ -16,20 +17,38 @@ sambamba_path = '/data/public/tools/bin/'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_bamfile", help="BAM file")
+parser.add_argument("bins_path", help="Bins path")
 parser.add_argument("-t","--threads", help = "Threads")
 parser.add_argument("-f","--fraction", help = "Fraction")
 args = parser.parse_args()
 
 #print args.input_bamfile
 input_bamfile = args.input_bamfile
+bins_path = args.bins_path
 if(args.threads):
     sambamba_threads = args.threads
 if(args.fraction):
     sambamba_fraction = args.fraction
 
+sample_name = os.path.splitext(input_bamfile)[0]
+print "Currently processing " + sample_name
+#Create folder
+if not os.path.isdir(sample_name):
+	os.mkdir(sample_name)
+#Goto folder
+os.chdir(sample_name)
+#shutil.move("../" + input_bamfile, ".")
+
 print "Subsampling BAM file"
-command = sambamba_path + "sambamba view " + input_bamfile + " -o " + os.path.splitext(input_bamfile)[0] + ".subsampled.bam -fbam -t" + str(sambamba_threads) + " -s" + str(sambamba_fraction) + " -p" 
+command = sambamba_path + "sambamba view ../" + input_bamfile + " -o " + sample_name + ".subsampled.bam -fbam -t" + str(sambamba_threads) + " -s" + str(sambamba_fraction) + " -p" 
 print "Command: " + command
-#call(command, shell = True)
+call(command, shell = True)
 
+command = "Rscript --vanilla qdnaseq_workflow.R " + sample_name + ".subsampled.bam " + bins_path
+print "Command: " + command
+call(command, shell = True)
 
+#Delete subsampled BAM
+shutil.rm(sample_name + ".subsampled.bam")
+os.chdir('..')
+quit()
